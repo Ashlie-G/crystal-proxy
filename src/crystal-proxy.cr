2313d9ac5db67f82bb3629bca3e0145d6d8a6133
Ashlie Gray <ashliejanegray@gmail.com>
2021-02-22 12:40:45 +1000

    outes estalished with kemal and functional

--- src/crystal-proxy.cr
+++ src/crystal-proxy.cr
@@ -1,12 +1,31 @@
 require "kemal"
 # TODO: Write documentation for `Crystal::Proxy`
+def get_code(env)
+  env.params.url["code"]
+end
+
 module Crystal::Proxy
   VERSION = "0.1.0"
 
-
 get "/" do
-  "Hello World!"
+  "Please refer to documentation for REST api specs"
 end
+
+get "/forward/:code" do |env|
+  url_code = get_code(env)
+  env.redirect "https://github.com"
+end
+post "/code/:code" do |env|
+  url_code = get_code(env)
+  "registering code #{url_code}"
+end
+
+delete "/code/:code" do |env|
+  url_code = get_code(env)
+  "deleting code #{url_code}"
+end
+
+
 
 Kemal.run
 end
